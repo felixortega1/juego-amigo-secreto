74a44b500e39b3c9f074c5c442a14475ae5765cc
FELIX <feog.2002@gmail.com>
2025-01-23 17:57:08 -0500

    actualiza app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 alert("Soy el programador");
+console.log("holaaaaaa");
