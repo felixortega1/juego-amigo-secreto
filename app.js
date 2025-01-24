adb2ae1781d9ce73be98361db95314fec7d4d703
FELIX <feog.2002@gmail.com>
2025-01-23 19:17:22 -0500

    crea funcion agregarAmigo()

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-alert("Soy el programador");
-console.log("holaaaaaa");
+let amigos = [];
+
+function limpiarCaja() {
+    document.getElementById("amigo").value = '';
+}
+
+function agregarAmigo(){
+    
+    if (document.getElementById('amigo').value !== '') {
+        //si no es vacio
+        amigos.push(document.getElementById("amigo").value);
+        console.log(amigos);
+        limpiarCaja();
+
+    } else {
+        //else-- si es vacio
+        alert("Por favor, inserte un nombre.");    
+    }  
+}
+
+/*
+function sortearAmigo() {
+    
+}
+*/
+
+
+
